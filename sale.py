bb58bad783d8701f9573ac5c13b2a4f5d1db93d7
jmartin <jmartin@zikzakmedia.com>
2014-07-11 11:43:46 +0200

    Change field delivery_date that override function field of the same name by field delivery_date_ that stores its value

--- sale.py
+++ sale.py
@@ -5,24 +5,33 @@ from trytond.model import fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
 
+
 __all__ = ['SaleLine']
 __metaclass__ = PoolMeta
 
 
 class SaleLine:
     __name__ = 'sale.line'
-    delivery_date = fields.Date('Delivery Date',
+    delivery_date_ = fields.Date('Delivery Date',
         states={
             'invisible': Eval('type') != 'line',
             'required': Eval('type') == 'line',
             }, depends=['type'])
 
-    def on_change_product(self):
-        res = super(SaleLine, self).on_change_product()
-        res['delivery_date'] = self.on_change_with_delivery_date()
-        return res
+    @classmethod
+    def __setup__(cls):
+        super(SaleLine, cls).__setup__()
+        if not cls.delivery_date.setter:
+            cls.delivery_date.setter = 'set_delivery_date'
+        if cls.delivery_date._field.readonly:
+            cls.delivery_date._field.readonly = False
 
-    def on_change_quantity(self):
-        res = super(SaleLine, self).on_change_quantity()
-        res['delivery_date'] = self.on_change_with_delivery_date()
-        return res
+    @fields.depends('delivery_date_')
+    def on_change_with_delivery_date(self, name=None):
+        if self.delivery_date_:
+            return self.delivery_date_
+        return super(SaleLine, self).on_change_with_delivery_date(name)
+
+    @classmethod
+    def set_delivery_date(cls, lines, name, value):
+        cls.write(lines, {'delivery_date_': value})
