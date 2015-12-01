916cf8b76d4716afd264508897c43305ec807461
resteve <resteve@zikzakmedia.com>
2015-12-01 11:18:37 +0100

    Tests

--- tests/test_sale_delivery_date_manual.py
+++ tests/test_sale_delivery_date_manual.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_delivery_date_manual module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends, test_view
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleDeliveryDateManualTestCase(unittest.TestCase):
+class SaleDeliveryDateManualTestCase(ModuleTestCase):
     'Test Sale Delivery Date Manual module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_delivery_date_manual')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_delivery_date_manual')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_delivery_date_manual'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleDeliveryDateManualTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
